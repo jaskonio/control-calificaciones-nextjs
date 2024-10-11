01f21d1472aec605a850d7f5c36a00ffc4341b4c
Jonatan Lopez <jonathan.lp17@gmail.com>
2024-10-11 20:39:58 +0200

    Delete repositories folder

--- src/repositories/interfaces/IRepository.ts
+++
@@ -1,7 +0,0 @@
-export interface IRepository<T> {
-    getAll(): Promise<T[]>;
-    getById(id: number): Promise<T | null>;
-    create(item: T): Promise<T>;
-    update(id: number, item: Partial<T>): Promise<T | null>;
-    delete(id: number): Promise<boolean>;
-}

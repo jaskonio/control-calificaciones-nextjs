b074e4299011a7c247cfc8f09ab71d262badf413
Jonatan Lopez <jonathan.lp17@gmail.com>
2024-10-13 19:47:53 +0200

    add course model

--- src/models/courses.ts
+++ src/models/courses.ts
@@ -1,6 +1,6 @@
 import { AcademicYear, Class, CourseStatus } from "@prisma/client";
 
-// Model to Create
+
 export interface CreateCourseModel {
   name: string;
   description: string;
@@ -9,11 +9,9 @@ export interface CreateCourseModel {
   status: CourseStatus
 }
 
-// Model to Update
 export interface UpdateCourseModel extends CreateCourseModel{
 }
 
-// Model To View
 export interface CourseViewModel {
   id: number;
   name: string;

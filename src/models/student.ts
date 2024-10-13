103c6a7a3a842bfb99e616d785570ea35e2750b2
Jonatan Lopez <jonathan.lp17@gmail.com>
2024-10-13 19:54:29 +0200

    Added Student Model

---
+++ src/models/student.ts
@@ -0,0 +1,30 @@
+import { AcademicYear, Enrollment, Parent, User } from "@prisma/client";
+
+export interface CreateStudentModel {
+    userId: number;
+    birthDate: string;
+    address: string;
+    phone: string;
+    enrollmentDate: string;
+    gradeLevel: string;
+    academicYearId: number
+}
+
+export interface UpdateStudentModel extends CreateStudentModel {
+}
+
+export interface StudenViewModel {
+    id: number;
+    userId: number;
+    birthDate: string;
+    address: string;
+    phone: string;
+    enrollmentDate: string;
+    gradeLevel: string;
+    academicYearId: number
+
+    user: User;
+    academicYear: AcademicYear;
+    parents: Parent[];
+    enrollments: Enrollment[];
+}

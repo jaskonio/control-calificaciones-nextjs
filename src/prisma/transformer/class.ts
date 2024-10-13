442e9b7bb4186b802ffaffbfd40542c88a8f5c69
Jonatan Lopez <jonathan.lp17@gmail.com>
2024-10-13 20:37:57 +0200

    Added Converter Class model

---
+++ src/prisma/transformer/class.ts
@@ -0,0 +1,39 @@
+import { ClassViewModel, CreateClassModel } from "@/models/class";
+import { Class } from "@prisma/client";
+
+
+export function ConverterClassModelToViewModel(model: Class & {
+    course: any;
+    subject: any;
+    teacher: any;
+    enrollments: any[];
+}): ClassViewModel {
+    return {
+        id: model.id,
+        courseId: model.courseId,
+        subjectId: model.subjectId,
+        teacherId: model.teacherId,
+        classroom: model.classroom,
+        startTime: model.startTime,
+        endTime: model.endTime,
+        dayOfWeek: model.dayOfWeek,
+
+        course: model.course,
+        subject: model.subject,
+        teacher: model.teacher,
+        enrollments: model.enrollments
+    };
+}
+
+
+export function ConverterClassInputToClassModel(input: ClassViewModel): CreateClassModel {
+    return {
+        courseId: input.courseId,
+        subjectId: input.subjectId,
+        teacherId: input.teacherId,
+        classroom: input.classroom,
+        startTime: input.startTime,
+        endTime: input.endTime,
+        dayOfWeek: input.dayOfWeek,
+    };
+}
